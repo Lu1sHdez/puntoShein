e38bf7bdef771c0f4774f1ea77f03f2aad7dd42d
AlfredoNava12 <20210650@uthh.edu.mx>
2025-01-28 00:23:16 -0600

    Agregue contenido a la línea de bienvenido

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hola Punto shein </h1>
+        <h1>Hola Punto shein 2025 </h1>
         <h2>Bienvenido a la pagina</h2>
       </header>
     </div>
