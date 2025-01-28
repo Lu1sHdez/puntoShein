c7dfa3e99d50c8946b2cfc8e1d9a1a458139465d
Lu1sHdez <luis2004hdez@gmail.com>
2025-01-27 22:31:32 -0600

    Agregar correctamente los archivos del frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hola Punto shein </h1>
+        <h2>Bienvenido a la pagina</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
