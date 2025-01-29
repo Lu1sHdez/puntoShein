a7f29278364b2d78e8dc7e63037fa927c6b94958
Lu1sHdez <luis2004hdez@gmail.com>
2025-01-28 21:24:27 -0600

    Creacion de funciones como login, logout, registro

---
+++ backend/src/libs/crearTokenAcceso.js
@@ -0,0 +1,12 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Cargar variables de entorno
+
+export const crearTokenAcceso = (usuario) => {
+  return jwt.sign(
+    { id: usuario.id, rol: usuario.rol }, // Payload del token
+    process.env.TOKEN_SECRET, // Clave secreta
+    { expiresIn: '1d' } // Expiración de 1 día
+  );
+};
---
+++ backend/src/models/usuario.model.js
@@ -0,0 +1,57 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../database/database.js';
+
+const Usuario = sequelize.define('Usuario', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  nombre_usuario: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  apellido_paterno: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  apellido_materno: {
+    type: DataTypes.STRING,
+  },
+  telefono: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      is: /^\d{10}$/, // Valida que sean exactamente 10 dígitos
+    },
+  },
+  correo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true, // Valida que sea un correo válido
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  rol: {
+    type: DataTypes.ENUM('usuario', 'administrador', 'empleado'),
+    defaultValue: 'usuario',
+  },
+}, {
+  tableName: 'usuarios',
+  timestamps: true, // Agrega automáticamente los campos createdAt y updatedAt
+});
+
+export default Usuario;
+
+
