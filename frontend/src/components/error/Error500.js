f69cf11e304d0015822ccdb7189a0e33c51d7244
Lu1sHdez <luis2004hdez@gmail.com>
2025-02-03 19:02:26 -0600

    Error500 imagen centrada

--- frontend/src/components/error/Error500.js
+++ frontend/src/components/error/Error500.js
@@ -4,21 +4,18 @@ import { Link } from "react-router-dom";
 const Error500 = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen px-6 text-center bg-black">
-      {/* Encabezado superior separado */}
-
-
       {/* Contenedor con fondo semitransparente */}
-      <div className="bg-gray-900 bg-opacity-90 p-10 rounded-lg shadow-2xl max-w-lg w-full">
+      <div className="bg-gray-900 bg-opacity-90 p-2 rounded-lg shadow-2xl max-w-lg w-full">
         {/* Imagen de error */}
         <img
           src="https://res.cloudinary.com/dgbs7sg9j/image/upload/v1738396258/e500_igczuw.webp" 
           alt="Error 500"
-          className="w-full max-w-xs sm:max-w-md h-auto mb-6 drop-shadow-lg"
+          className="w-full max-w-xs sm:max-w-rem h-auto mb-6 drop-shadow-lg mx-auto" // Agregado mx-auto
         />
 
         {/* Mensaje de error */}
         <h2 className="text-3xl md:text-4xl font-bold text-white">
-        ¡Error Interno del Servidor!
+          ¡Error Interno del Servidor!
         </h2>
         <p className="text-gray-300 mt-2 max-w-md text-lg">
           Ocurrió un error inesperado en el servidor. <br />
