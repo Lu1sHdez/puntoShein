5bef04c19804a3b97f147807fd9d18c0b655fa86
Lu1sHdez <luis2004hdez@gmail.com>
2025-02-04 18:23:56 -0600

    Cambio de bd

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,16 +3,16 @@ import { sequelize } from './database/database.js';
 
 async function main() {
   try {
-    console.log("🔄 Conectando a la base de datos...");
+    console.log("Conectando a la base de datos...");
     await sequelize.authenticate();
-    console.log("✅ Conexión establecida con Neon PostgreSQL");
+    console.log("Conexión establecida con Neon PostgreSQL");
 
     const port = process.env.PORT || 4000;
     app.listen(port, () => {
-      console.log(`🚀 Servidor corriendo en el puerto ${port}`);
+      console.log(`Servidor corriendo en el puerto ${port}`);
     });
   } catch (error) {
-    console.error("❌ Error al conectar con la base de datos:", error);
+    console.error("Error al conectar con la base de datos:", error);
   }
 }
 
