05e3e81cdc27e7bcdf9fd255586ad6418df1a632
Lu1sHdez <luis2004hdez@gmail.com>
2025-03-26 23:38:48 -0600

    fix: dependencias

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -3,14 +3,31 @@ import dotenv from 'dotenv';
 
 dotenv.config(); // Cargar variables de entorno
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER, 
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
+let sequelize;
+
+if (process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    port: process.env.DB_PORT || 5432,
-    logging: false, 
-  }
-);
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'postgres',
+      port: process.env.DB_PORT || 5432,
+      logging: false,
+    }
+  );
+}
+
+export { sequelize };
