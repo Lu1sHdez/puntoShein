ad51e95524bf86429c65e2b3ec069eefb5fa01bf
Lu1sHdez <luis2004hdez@gmail.com>
2025-03-26 19:26:26 -0600

    fix: dependencias

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -12,5 +12,11 @@ export const sequelize = new Sequelize(
     dialect: 'postgres',
     port: process.env.DB_PORT || 5432,
     logging: false, 
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 );
